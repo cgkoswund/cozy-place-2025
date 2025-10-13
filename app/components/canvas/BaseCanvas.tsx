990e65118e88c97f5cb8ec7dcb05883db26e7059
cgkoswund <tnyaffram@gmail.com>
2025-10-13 01:47:24 +0000

    adding r3f performance tracking

--- app/components/canvas/BaseCanvas.tsx
+++ app/components/canvas/BaseCanvas.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import CameraAnimations from "./CameraAnimations";
 import CatRoom from "./CatRoom";
 import Lights from "./Lights";
+import { Perf } from "r3f-perf";
 
 const BaseCanvas: React.FC = () => {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas camera={{ position: [0, 2.5 / 8, 6 / 4] }}>
+        <Perf position="top-left" />
         <Lights />
         <axesHelper />
         <CatRoom />
